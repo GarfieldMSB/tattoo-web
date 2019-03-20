6421af66d06da664caf2f3a43d281a8ab43bfe01
alumnossur <alumnossur@inacap.cl>
2019-03-20 15:29:13 -0300

    init commit

---
+++ build/generated/src/org/apache/jsp/index_jsp.java
@@ -0,0 +1,82 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class index_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\n");
+      out.write("\n");
+      out.write("\n");
+      out.write("<!DOCTYPE html>\n");
+      out.write("<html>\n");
+      out.write("    <head>\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\n");
+      out.write("        <title>Tattoo</title>\n");
+      out.write("        <link rel=\"stylesheet\" href=\"css/style.css\"/>\n");
+      out.write("    </head>\n");
+      out.write("    <body>\n");
+      out.write("        <header>\n");
+      out.write("            <input type=\"checkbox\" id=\"btn-menu\">\n");
+      out.write("            <label for=\"btn-menu\"><img src=\"images/MenuLateral.png\" alt=\"\"/></label>\n");
+      out.write("            <nav class=\"menu\">\n");
+      out.write("                <ul>\n");
+      out.write("                    <li><a href=\"\">Perfil</a></li>\n");
+      out.write("                    <li><a href=\"\">Categorias</a></li>\n");
+      out.write("                    <li><a href=\"\">Consejos</a></li>\n");
+      out.write("                    <li><a href=\"\">Salir</a></li>\n");
+      out.write("                </ul>\n");
+      out.write("            </nav>\n");
+      out.write("        </header>\n");
+      out.write("        <!--<img src=\"Proyect Web Page Tattoo.png\" id=\"Proyect Web Page Tattoo.png\" /> -->\n");
+      out.write("    </body>\n");
+      out.write("</html>\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
